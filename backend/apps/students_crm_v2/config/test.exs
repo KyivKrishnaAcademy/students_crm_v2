5fb4d07f2660d9dcba68f368aab856e04b278f7c
Maksym Pugach <pugach.m@gmail.com>
2017-12-04 07:10:33 +0200

    [backend] make tests to launch

--- backend/apps/students_crm_v2/config/test.exs
+++ backend/apps/students_crm_v2/config/test.exs
@@ -3,8 +3,5 @@ use Mix.Config
 # Configure your database
 config :students_crm_v2, StudentsCrmV2.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "students_crm_v2_test",
-  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
