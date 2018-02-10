2ca31c6a99b8b689e1621f5e07845cc04f24f292
Maksym Pugach <pugach.m@gmail.com>
2018-02-10 08:05:35 +0200

    [backend][client] /users requires authentication

--- backend/apps/students_crm_v2_web/lib/students_crm_v2_web/controllers/user_controller.ex
+++ backend/apps/students_crm_v2_web/lib/students_crm_v2_web/controllers/user_controller.ex
@@ -3,6 +3,10 @@ defmodule StudentsCrmV2Web.UserController do
 
   action_fallback StudentsCrmV2Web.FallbackController
 
+  plug Guardian.Plug.EnsureAuthenticated,
+    module: StudentsCrmV2Web.Guardian,
+    error_handler: StudentsCrmV2Web.AuthErrorHandler
+
   def index(conn, _params) do
     render(conn, "index.json-api", data: StudentsCrmV2.list_users())
   end
--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,7 +1,9 @@
 import config from 'students-crm-v2/config/environment';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default JSONAPIAdapter.extend({
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: config.API_HOST,
   namespace: config.API_NAMESPACE,
+  authorizer: 'authorizer:token',
 });
