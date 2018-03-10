b92ccdc6a314569e2e6d3e7ced9128331a2a10f8
Maksym Pugach <pugach.m@gmail.com>
2018-03-10 13:31:16 +0200

    [client] login mthod chooser

--- client/app/locales/uk/translations.js
+++ client/app/locales/uk/translations.js
@@ -1,6 +1,8 @@
 export default {
   'header-toolbar.login': 'Увійти',
 
+  'login.index.chooseMethod': 'Оберіть метод для входу',
+
   'main-sidenav.home': 'Домашня',
   'main-sidenav.logout': 'Вийти',
   'main-sidenav.users': 'Люди',
