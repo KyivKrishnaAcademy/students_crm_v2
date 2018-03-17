704ce685ec68004467226fd20eeed2670840781c
Maksym Pugach <pugach.m@gmail.com>
2018-03-17 14:49:17 +0200

    [client] add RTL and pluralForm configuration to uk

---
+++ client/app/locales/uk/config.js
@@ -0,0 +1,12 @@
+export default {
+  rtl: true,
+
+  pluralForm: function(count) {
+    if (count === 0) { return 'zero'; }
+    if (count === 1) { return 'one'; }
+    if (count === 2) { return 'two'; }
+    if (count < 5) { return 'few'; }
+    if (count >= 5) { return 'many'; }
+    return 'other';
+  },
+}
