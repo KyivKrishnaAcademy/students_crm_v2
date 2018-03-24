569d03777303cabef5b4000ce61512603a22ee5e
Maksym Pugach <pugach.m@gmail.com>
2018-03-24 08:10:15 +0200

    [client] sort translations alphabetically

--- client/app/locales/uk/translations.js
+++ client/app/locales/uk/translations.js
@@ -3,15 +3,15 @@ var PRIVACY_AGREEMENT = 'Згода на обробку персональних
 export default {
   'header-toolbar.login': 'Увійти',
 
-  'login.chooseMethod': 'Оберіть метод для входу',
-  'login.enterToken': 'Введіть токен',
-
   'login-step/telegram.continue': 'Продовжити',
   'login-step/telegram.enterToken': 'Введіть токен',
   'login-step/telegram.goTalkWith': 'Поспілкуйтеся з нашим',
   'login-step/telegram.tokenReuired': 'Небхідно заповнити',
   'login-step/telegram.toObtainKey': 'щоб отримати токен для входу',
   'login-step/telegram.unauthorized': 'Такого токену не існує або його термін придатності сплив',
+
+  'login.chooseMethod': 'Оберіть метод для входу',
+  'login.enterToken': 'Введіть токен',
 
   'main-sidenav.home': 'Домашня',
   'main-sidenav.logout': 'Вийти',
