7c2f796dfba245a02c4b7c6fab0006c80f5dd769
Maksym Pugach <pugach.m@gmail.com>
2018-03-25 12:30:28 +0300

    [client] skip privacy agreement step if it is completed already

--- client/app/pods/register/controller.js
+++ client/app/pods/register/controller.js
@@ -5,6 +5,8 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
   currentUser: service(),
 
+  isInvalid: false,
+
   actions: {
     agreeToPrivacy(nextStep) {
       this.get('currentUser.user').agreeToPrivacyPolicy().then(() => nextStep());
--- client/app/pods/register/route.js
+++ client/app/pods/register/route.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
+import { inject as service } from '@ember/service';
+
 export default Route.extend({
+  currentUser: service(),
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    let step = + this.get('currentUser.user.privacyAgreed');
+
+    controller.set('currentStep', step);
+  }
 });
