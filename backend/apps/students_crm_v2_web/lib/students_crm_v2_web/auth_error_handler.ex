460386a5eee6ffe8815f048200d34e2c35632b92
Maksym Pugach <pugach.m@gmail.com>
2018-07-06 17:42:17 +0300

    remove leftovers from AuthErrorHandler

--- backend/apps/students_crm_v2_web/lib/students_crm_v2_web/auth_error_handler.ex
+++ backend/apps/students_crm_v2_web/lib/students_crm_v2_web/auth_error_handler.ex
@@ -4,5 +4,5 @@ defmodule StudentsCrmV2Web.AuthErrorHandler do
   import Plug.Conn
 
   def auth_error(conn, {type, _reason}, _opts),
-    do: send_resp(conn, 401, Poison.encode!(%{message: to_string(type), ololo: true}))
+    do: send_resp(conn, 401, Poison.encode!(%{message: to_string(type)}))
 end
