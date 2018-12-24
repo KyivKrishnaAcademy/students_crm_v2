c0bdd2aac4ef392e125493e7bf839e58503405d9
Maksym Pugach <pugach.m@gmail.com>
2018-12-24 07:34:12 +0200

    [#24] fix failing tests and add some

--- backend/apps/login_token_cache/test/login_token_cache_test.exs
+++ backend/apps/login_token_cache/test/login_token_cache_test.exs
@@ -2,7 +2,15 @@ defmodule LoginTokenCacheTest do
   use ExUnit.Case
   doctest LoginTokenCache
 
-  test "greets the world" do
-    assert LoginTokenCache.hello() == :world
+  test "generate" do
+    assert LoginTokenCache.generate("ololo") =~ ~r/\w{5}/
+  end
+
+  test "get" do
+    value = "ololo"
+    key = LoginTokenCache.generate(value)
+
+    assert LoginTokenCache.get(key) == {:ok, value}
+    assert LoginTokenCache.get(key) == {:error, :no_token_found}
   end
 end
--- backend/apps/students_crm_v2_web/test/students_crm_v2_web/controllers/page_controller_test.exs
+++ backend/apps/students_crm_v2_web/test/students_crm_v2_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule StudentsCrmV2Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "container"
   end
 end
